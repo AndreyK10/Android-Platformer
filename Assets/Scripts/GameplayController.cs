868ca5570c3437e59e0385a92d89cd43e69d53d7
Andrey Kostyuk <andrey.kostyuk10@gmail.com>
2021-08-19 00:06:03 +0300

    level ui

---
+++ Assets/Scripts/GameplayController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    [SerializeField] private Animator animFade, animTextFinish, animTextGO;
+    [SerializeField] private GameObject pauseScreen, gameplayUI;
+    [SerializeField] private float timeBeforeText;
+
+
+
+    private void Update()
+    {
+        if (PlayerController.isFinished)
+        {
+            StartCoroutine(Finish(animTextFinish));
+        }
+        if (PlayerController.isDead)
+        {
+            StartCoroutine(Finish(animTextGO));
+        }
+
+    }
+
+    private IEnumerator Finish(Animator anim)
+    {
+        animFade.SetTrigger("Start");
+        yield return new WaitForSeconds(timeBeforeText);
+        anim.SetTrigger("Start");
+    }    
+
+    private void SwitchPause(float _timeScale, bool _pauseScreen, bool _gameplayUI)
+    {
+        Time.timeScale = _timeScale;
+        gameplayUI.gameObject.SetActive(_gameplayUI);
+        pauseScreen.gameObject.SetActive(_pauseScreen);
+    }
+
+    public void PauseGame()
+    {
+        SwitchPause(0f, true, false);
+    }
+
+    public void ContinueGame()
+    {
+        SwitchPause(1f, false, true);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMovement))]
 public class PlayerController : MonoBehaviour
 {
     public static bool isDead;
-    public GameObject cinemachineCamera;
+    public static bool isFinished;
+    [SerializeField] private GameObject cinemachineCamera;
+    private PlayerMovement playerM;
 
+
+    private void Awake()
+    {
+        playerM = GetComponent<PlayerMovement>();
+        //playerM.enabled = false;
+    }
     private void Start()
     {
         isDead = false;
+        isFinished = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,6 +26,21 @@ public class PlayerController : MonoBehaviour
         {
             cinemachineCamera.SetActive(false);
             isDead = true;
+        }
+        if (collision.CompareTag("Activator"))
+        {
+            collision.gameObject.SetActive(false);
+            playerM.enabled = true;
+        }
+        if (collision.CompareTag("FinalCutScene"))
+        {
+            collision.gameObject.SetActive(false);
+            cinemachineCamera.SetActive(false);
+            playerM.enabled = false;
+        }
+        if (collision.CompareTag("Finish"))
+        {
+            isFinished = true;
         }
     }
 }
